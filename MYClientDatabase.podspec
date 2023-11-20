8343991ae2e3e7ab3ab203cde32c21ef4d950fb1
温明妍 <wenmy@tuya.com>
2023-11-20 17:20:42 +0800

    add sqlite

--- MYClientDatabase.podspec
+++ MYClientDatabase.podspec
@@ -22,6 +22,5 @@ TODO: Add long description of the pod here.
     'MYClientDatabase' => ['MYClientDatabase/Assets/**/*.sqlite']
   }
    
-  # s.frameworks = 'UIKit', 'MapKit'
-  # s.dependency 'AFNetworking', '~> 2.3'
+   s.dependency 'FMDB'
 end
--- MYClientDatabase/Classes/MYClientDatabase.m
+++ MYClientDatabase/Classes/MYClientDatabase.m
@@ -6,13 +6,18 @@
 //
 
 #import "MYClientDatabase.h"
-#import <sqlite3.h>
+#import <fmdb/FMDB.h>
 
 NSString *kDatabaseName = @"database.sqlite";
 
-@implementation MYClientDatabase {
-    sqlite3 *_db;
-}
+@interface MYClientDatabase ()
+
+@property (nonatomic, assign) BOOL openSuccess;
+@property (nonatomic, strong) FMDatabase *database;
+
+@end
+
+@implementation MYClientDatabase
 
 + (instancetype)database {
     static id instance = nil;
@@ -34,22 +39,36 @@ NSString *kDatabaseName = @"database.sqlite";
 }
 
 - (NSArray<MYDataChatPerson *> *)getAllChatPersonWithUserId:(long long)userId {
-    
+    FMResultSet *resultSet = [self.database executeQuery:@"SELECT userId,username,icon FROM tb_user where affUserId=?",userId];
     //TODO: wmy
-    return nil;
+    NSMutableArray<MYDataChatPerson *> *chatPersons = [NSMutableArray array];
+    while (resultSet.next) {
+        MYDataChatPerson *person = [[MYDataChatPerson alloc] init];
+        person.userId = [resultSet intForColumn:@"userId"];
+        person.name = [resultSet stringForColumn:@"username"];
+        person.iconURL = [resultSet stringForColumn:@"icon"];
+    }
+    return chatPersons;
 }
 
 - (NSArray<MYDataMessage *> *)getChatMessageWithPerson:(long long)userId {
     return nil;
 }
 
+- (NSString *)docDBFilePath {
+    NSArray *path = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *docDirectory = [path objectAtIndex:0];
+    NSString *dstPath = [docDirectory stringByAppendingPathComponent:kDatabaseName];
+    return dstPath;
+}
+
 - (void)copyDatabaseToHomeDirectory {
     //TODO: wmy 将sqlite放到home目录下
-//    NSURL *url = [[NSBundle bundleForClass:self.class] URLForResource:@"MYClientDatabase" withExtension:@"bundle"];
-//    NSBundle *resourceBundle = [NSBundle bundleWithURL:url];
-//    NSString *path = [resourceBundle pathForResource:kDatabaseName ofType:nil];
-    NSLog(@"path");
-    UIImage *image = [UIImage imageNamed:@"MYClientDatabase.bundle/DCIM_1657090588"];
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:kDatabaseName ofType:nil];
+    NSString *dstPath = [self docDBFilePath];
+    NSLog(@"dstPath = %@",dstPath);
+    NSError *error;
+    [[NSFileManager defaultManager] copyItemAtPath:filePath toPath:dstPath error:&error];
     NSLog(@"");
 }
 
@@ -58,21 +77,11 @@ NSString *kDatabaseName = @"database.sqlite";
     // _db是数据库的句柄,即数据库的象征,如果对数据库进行增删改查,就得操作这个示例
     
     // 获取数据库文件的路径
-//    NSString *docPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
-    NSString *docPath = [[NSBundle mainBundle] pathForResource:[NSString stringWithFormat:@"MYClientDatabase.bundle/%@",kDatabaseName] ofType:nil];
-    
-    NSString *fileName = docPath;
-    NSLog(@"fileNamePath = %@",fileName);
-    // 将 OC 字符串转换为 C 语言的字符串
-    const char *cFileName = fileName.UTF8String;
-    
-    // 打开数据库文件(如果数据库文件不存在,那么该函数会自动创建数据库文件)
-    int result = sqlite3_open(cFileName, &_db);
-    
-    if (result == SQLITE_OK) {  // 打开成功
-        NSLog(@"成功打开数据库");
-    } else {
-        NSLog(@")打开数据库失败");
+    NSString *docPath = [self docDBFilePath];
+    self.database = [FMDatabase databaseWithPath:docPath];
+    if (!self.database.isOpen) {
+        self.openSuccess = self.database.open;
+        NSLog(@"数据库打开成功");
     }
 }
 
